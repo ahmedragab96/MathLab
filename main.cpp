1b0bf574edbef5077edb88f1245daa48d0c8a55f
Ahmed Ragab <33171138+ahmedragab96@users.noreply.github.com>
2017-12-01 23:46:10 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 #include <string>
-#include <cstdio>
-#include "stdarg.h"
-#include <math.h>
-#include "Matrix.h"
-#include "matlab.h"
-#include <cstring>
+#include <sstream>
+#include "CColor.h"
+using namespace std ;
 
-using namespace std;
+int main () {
 
 
-int main (int argc , char* argv[]){
-	
-	Matlab myMatlab1;
-	if(argc > 1)
-	{
-		myMatlab1.loadfile("D://testmatrix.txt");
-	}
-	else
-	{
-		myMatlab1.loadfile("D://testmatrix.txt");
-	}
-	system("pause");
-	return 0;
+	CColor a(255,128,192);
+	//CColor b ;
+	cout << a << endl;
+	//cout << b << endl;
+	//CColor c(0x00667755);
 }
-
